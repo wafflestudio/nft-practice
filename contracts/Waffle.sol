a3e9a1142564858957121caad43a0d3ff4c9a7f3
j-min5u <ms.cheong@bucketplace.net>
2022-01-21 23:24:09 +0900

    feat: Waffle.sol core 로직 재정립

--- contracts/Waffle.sol
+++ contracts/Waffle.sol
@@ -7,28 +7,27 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // @dev 와플 NFT 코어 컨트랙트. 프로퍼티 정의 및 실행 로직 구성
 contract Waffle is Ownable {
 
-    /**
-        title: 와플 이름
-        size: 와플 크기 (1부터 시작)
-        baseColor: 와플 빵 색
-        verticals: 가로 줄
-        horizontals: 세로 줄
-        toppings: 와플 토핑 (토큰 dna)
+    enum Flavor { Plain, Chocolate, Vanilla }
 
+    /**
+        name: 와플 이름
+        flavour: 와플 종류 (rendering 시 색상 등 결정)
+        horizontals: 가로 줄 위치
+        verticals: 세로 줄 위치
         === verticals & horizontals
+        7*7 격자 상에 서로 수직인 평행선 두세트
         - msb => 1~5 (선 긋는 위치)
-         가로 * 세로 선 긋는 조합 = 100 가지 ( 1~5 중 2개, 5C2 = 10 )
+         가로 * 세로 선 긋는 조합 = 36가지
         - lsb => 색상 16개 (x0 ~xF)
         - (uint8) horizontals[0] = x01 ~ xF5
+        서로 겹치거나 인접할 수 없음
         @dev TODO 프로퍼티 확장 및 애플리케이션 형태를 고려한 도메인 설계
     */
     struct MetaData {
-        string title;
-        uint8 size;
-        uint8 baseColor;
-        uint8[2] verticals;
+        string name;
+        Flavor flavor;
         uint8[2] horizontals;
-        uint256 toppings;
+        uint8[2] verticals;
     }
 
     MetaData[] public waffles;
@@ -42,34 +41,69 @@ contract Waffle is Ownable {
         _currentBaseURI = baseURI;
     }
 
+
+
     /**
         baseColor, size 결정 로직 (?)
         @dev TODO 새로운 와플 발행, 발행 세부 로직 고도화
     */
-    function _createRandomWaffle(string memory title) internal returns (uint) {
-        uint8 size = 1;
-        uint8 baseColor = 1;
-        uint256 toppings = _getRandom(title);
-        uint8[2] memory verticals;
-        verticals[0] = _getRandomLine(title);
-        verticals[1] = _getRandomLine(title);
-        uint8[2] memory horizontals;
-        horizontals[0] = _getRandomLine(title);
-        horizontals[1] = _getRandomLine(title);
-        waffles.push(MetaData(title, size, baseColor, verticals, horizontals, toppings));
-        uint tokenId = waffles.length - 1;
+    function _createWaffle(
+        string memory name,
+        uint8[2] memory horizontals,
+        uint8[2] memory verticals
+    ) internal returns (uint) {
+        require(!_validateShape(horizontals, verticals), 'this shape is not a waffle at all!');
+
+        Flavor flavor = _getRandomFlavour(name);
+        waffles.push(MetaData(name, flavor, horizontals, verticals));
+
+        uint tokenId = _encodeShape(horizontals, verticals);
         waffleToOwner[tokenId] = msg.sender;
         ownerWaffleCount[msg.sender]++;
         return tokenId;
     }
 
-    function _getRandomLine(string memory title) internal returns(uint8) {
-        return uint8(_getRandom(title)) % 5 + 1;
+    function _validateShape(
+        uint8[2] memory horizontals,
+        uint8[2] memory verticals
+    ) internal returns (bool) {
+        if(verticals[0] >= verticals[1]) return false;
+        if(horizontals[0] >= horizontals[1]) return false;
+        uint8 vDistance = verticals[1] - verticals[0];
+        uint8 hDistance = horizontals[1] - horizontals[0];
+        if(vDistance == 1 || hDistance == 1) return false;
+        return true;
     }
 
+    function _encodeShape(
+        uint8[2] memory horizontals,
+        uint8[2] memory verticals
+    ) internal returns (uint8) {
+        uint8 hor = _encodePair(horizontals[0] - 1, horizontals[1] - 2);
+        uint8 ver = _encodePair(verticals[0] - 1, verticals[1] - 2);
+        return hor*6 + ver; //return int of range 0 to 35
+    }
+
+    function _encodePair(
+        uint8 memory first,
+        uint8 memory second
+    ) internal returns (uint8) {
+        require(first>=0 && first<3, 'line out of range');
+        require(second>=1 && second<4, 'line out of range');
+        if(first == 0) return second -1;
+        if(first == 1) return second +1;
+        else return 5;
+    }
+
+    function _getRandomFlavour(string memory name) internal returns (Flavor) {
+        uint8 randomNum = _getRandom(name) % 3;
+        if(randomNum == 0) return Flavor.Plain;
+        if(randomNum == 1) return Flavor.Chocolate;
+        else return Flavor.Vanilla;
+    }
 
     // @dev pseudo random 함수
-    function _getRandom(string memory title) internal returns(uint256) {
-        return uint(keccak256(abi.encode(block.timestamp, msg.sender, title)));
+    function _getRandom(string memory name) internal returns(uint256) {
+        return uint(keccak256(abi.encode(block.timestamp, msg.sender, name)));
     }
 }
